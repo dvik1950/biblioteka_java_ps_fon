d98530d2c284f012317d1601f28df66b072df41e
Viktor <despic.viktor@gmail.com>
2019-06-12 14:01:36 +0200

    Obriši nepotrebne komentare

--- ZajednickiProjekatB/src/domen/Knjiga.java
+++ ZajednickiProjekatB/src/domen/Knjiga.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domen;
 
-import java.io.Serializable;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author FON
- */
 public class Knjiga extends OpstiDomenskiObjekat{
     private String ISBN;
     private String nazivKnjige;
