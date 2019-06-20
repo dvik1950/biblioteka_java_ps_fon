8ea465da0be038c9140cec8ec618587598cf5e2b
Viktor <despic.viktor@gmail.com>
2019-06-20 14:49:18 +0200

    Obriši nepotrebne komenatre i importe

--- ZajednickiProjekatB/src/domen/OpstiDomenskiObjekat.java
+++ ZajednickiProjekatB/src/domen/OpstiDomenskiObjekat.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package domen;
 
 import java.io.Serializable;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-
 
 public abstract class OpstiDomenskiObjekat implements Serializable {
     
     public abstract String vratiImeTabele();
-    public abstract String vratiParametre();  //values u insertu
+    public abstract String vratiParametre();
     public abstract String vratiPK();
     public abstract String vratiVrednostPK();
     public abstract List<OpstiDomenskiObjekat> RSuTabelu(ResultSet rs);
